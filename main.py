1c759f3c2eb83f4d7f5ffd26c2bdd021de33d4d9
Jinyeong Choi <choiwlsd@gmail.com>
2026-06-02 19:30:07 +0900

    Change: criteria for pose analysis

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from pose.pose_agent import PoseAgent
 
 if __name__ == "__main__":
     project_root = Path(__file__).resolve().parent
-    source = cv2.VideoCapture(filename=str(project_root / "asset" / "input_bad.mp4"))
+    source = cv2.VideoCapture(filename=str(project_root / "asset" / "input_good.mp4"))
     # source = cv2.VideoCapture(0)  # 웹캠 사용 시
     source.set(cv2.CAP_PROP_BUFFERSIZE, 1)
 
